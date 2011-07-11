2329369c3b67a84f133dc5c0839f62cd6a0028f0
Anton Yabchinskiy <arn@users.berlios.de>
2011-07-11 14:12:15 +0400

    Packet extraction functions moved to extractor

--- src/ealt_extractor.erl
+++ src/ealt_extractor.erl
@@ -132,7 +132,7 @@ handle_cast(_Msg, State) ->
 %%--------------------------------------------------------------------
 handle_info({tcp, _Socket, Bytes}, State = #state{buffer = Buffer}) ->
     State_1 = State#state{buffer = <<Buffer/bytes, Bytes/bytes>>},
-    State_2 = extract_packets(State_1),
+    State_2 = read_packets(State_1),
     Timeout = State_2#state.refresh_rate,
     {noreply, State_2, Timeout};
 handle_info({Reason = tcp_closed, _Socket}, State) ->
@@ -179,26 +179,6 @@ code_change(_Old_Vsn, State, _Extra) ->
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
-%% Extract packets from buffer while there are any.
-%%
-%% @spec extract_packets(State :: #state{}) -> State_1 :: #state{}
-%% @end
-%%--------------------------------------------------------------------
-extract_packets(State = #state{buffer = Buffer}) ->
-    case ealt_packets:read_packet(Buffer) of
-        {ok, Packet, Buffer_1} ->
-            ?debugFmt("Packet ~p extracted.", [Packet]),
-            %% ealt_decoder:process_packet(Packet),
-            State_1 = State#state{buffer = Buffer_1},
-            State_2 = handle_special_packet(Packet, State_1),
-            extract_packets(State_2);
-        {error, no_match} ->
-            State
-    end.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
 %% Handles refresh rate and keyframe packets. Modifies <em>State</em>
 %% accordingly.
 %%
@@ -236,6 +216,30 @@ handle_special_packet(#packet{car_id = 0,
     end;
 handle_special_packet(_Packet, State) ->
     State.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% `true' if packet of specified <em>Type</em> with <em>Car_Id</em>
+%% has encrypted payload.
+%%
+%% @spec is_payload_encrypted(Car_Id :: non_neg_integer(), Type :: non_neg_integer()) -> boolean()
+%% @end
+%%--------------------------------------------------------------------
+is_payload_encrypted(_Car_Id = 0, _Type = ?SYSTEM_PACKET_EVENT_ID) -> false;
+is_payload_encrypted(_Car_Id = 0, _Type = ?SYSTEM_PACKET_KEYFRAME) -> false;
+is_payload_encrypted(_Car_Id = 0, _Type = ?SYSTEM_PACKET_VALID_MARKER) -> false;
+is_payload_encrypted(_Car_Id = 0, _Type = ?SYSTEM_PACKET_COMMENTARY) -> true;
+is_payload_encrypted(_Car_Id = 0, _Type = ?SYSTEM_PACKET_REFRESH_RATE) -> false;
+is_payload_encrypted(_Car_Id = 0, _Type = ?SYSTEM_PACKET_NOTICE) -> true;
+is_payload_encrypted(_Car_Id = 0, _Type = ?SYSTEM_PACKET_TIMESTAMP) -> true;
+is_payload_encrypted(_Car_Id = 0, _Type = ?SYSTEM_PACKET_WEATHER) -> true;
+is_payload_encrypted(_Car_Id = 0, _Type = ?SYSTEM_PACKET_SPEED) -> true;
+is_payload_encrypted(_Car_Id = 0, _Type = ?SYSTEM_PACKET_TRACK_STATUS) -> true;
+is_payload_encrypted(_Car_Id = 0, _Type = ?SYSTEM_PACKET_COPYRIGHT) -> false;
+is_payload_encrypted(Car_Id, _Type = ?CAR_PACKET_POSITION_UPDATE) when Car_Id > 0 -> false;
+is_payload_encrypted(Car_Id, _Type = ?CAR_PACKET_POSITION_HISTORY) when Car_Id > 0-> true;
+is_payload_encrypted(Car_Id, Type) when Car_Id > 0, Type >= 1, Type =< 13 -> true.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -278,3 +282,167 @@ keyframe_url(0) ->
 keyframe_url(Keyframe_Id) ->
     lists:flatten(io_lib:format("http://~s/keyframe_~5..0B.bin",
                                 [?LIVE_TIMING_HOST, Keyframe_Id])).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Packet with long payload has no packet <em>data</em>. It's `value'
+%% field is the number of bytes to be extracted from the
+%% <em>bytes</em> stream to get the <em>payload</em>.
+%%
+%% @spec read_long_payload(Value :: integer(), Bytes :: binary()) ->
+%%     {ok, Data :: integer(), Payload :: binary(), Other_Bytes :: binary()} |
+%%     {error, no_match}
+%% @end
+%%--------------------------------------------------------------------
+read_long_payload(Value, Bytes) ->
+    case Bytes of
+        <<Payload:Value/bytes, Other_Bytes>> ->
+            {ok, 0, Payload, Other_Bytes};
+        _ ->
+            {error, no_match}
+    end.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Extracts packet from stream of <em>Bytes</em>.
+%%
+%% @spec read_packet(Bytes :: binary()) ->
+%%     {ok, Packet, Other_Bytes :: binary} |
+%%     {error, Reason :: term()}
+%% where Reason = no_match
+%% @end
+%%--------------------------------------------------------------------
+read_packet(<<Type_1:3, Car_Id:5, Value:7, Type_2:1, Bytes/bytes>>) ->
+    Type = Type_1 bor (Type_2 bsl 3),
+    read_packet(Car_Id, Type, Value, Bytes);
+read_packet(_Bytes) ->
+    {error, no_match}.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Extracts packet payload and constructs packet.
+%%
+%% @spec read_packet(Car_Id :: non_neg_integer(),
+%%                   Type :: non_neg_integer(),
+%%                   Value :: non_neg_integer(),
+%%                   Bytes :: binary()) ->
+%%     {ok, Packet, Other_Bytes :: binary} |
+%%     {error, no_match}
+%% @end
+%%--------------------------------------------------------------------
+read_packet(Car_Id, Type, Value, Bytes) ->
+    ?debugFmt("Reading packet, car id ~p, type ~p, value ~p, bytes ~P.",
+              [Car_Id, Type, Value, Bytes, 128]),
+    Encrypted = is_payload_encrypted(Car_Id, Type),
+    Read_Payload = read_payload_fun(Car_Id, Type),
+    case Read_Payload(Value, Bytes) of
+        {ok, Data, Payload, Other_Bytes} ->
+            Packet = #packet{car_id = Car_Id,
+                             type = Type,
+                             data = Data,
+                             payload = Payload,
+                             encrypted = Encrypted},
+            {ok, Packet, Other_Bytes};
+        Error = {error, _Reason} ->
+            Error
+    end.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Extract packets from buffer while there are any.
+%%
+%% @spec extract_packets(State :: #state{}) -> State_1 :: #state{}
+%% @end
+%%--------------------------------------------------------------------
+read_packets(State = #state{buffer = Buffer}) ->
+    case read_packet(Buffer) of
+        {ok, Packet, Buffer_1} ->
+            ?debugFmt("Packet ~p extracted.", [Packet]),
+            %% ealt_decoder:process_packet(Packet),
+            State_1 = State#state{buffer = Buffer_1},
+            State_2 = handle_special_packet(Packet, State_1),
+            read_packets(State_2);
+        {error, no_match} ->
+            State
+    end.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Payload extraction function for packet of type <em>Type</em> with <em>Car_Id</em>.
+%%
+%% @spec read_payload_fun(Car_Id :: non_neg_integer(), Type :: non_neg_integer()) -> fun()
+%% @end
+%%--------------------------------------------------------------------
+read_payload_fun(_Car_Id = 0, _Type = ?SYSTEM_PACKET_EVENT_ID) ->
+    fun read_some_payload/2;
+read_payload_fun(_Car_Id = 0, _Type = ?SYSTEM_PACKET_KEYFRAME) ->
+    fun read_some_payload/2;
+read_payload_fun(_Car_Id = 0, _Type = ?SYSTEM_PACKET_VALID_MARKER) ->
+    fun read_zero_payload/2;
+read_payload_fun(_Car_Id = 0, _Type = ?SYSTEM_PACKET_COMMENTARY) ->
+    fun read_long_payload/2;
+read_payload_fun(_Car_Id = 0, _Type = ?SYSTEM_PACKET_REFRESH_RATE) ->
+    fun read_zero_payload/2;
+read_payload_fun(_Car_Id = 0, _Type = ?SYSTEM_PACKET_NOTICE) ->
+    fun read_long_payload/2;
+read_payload_fun(_Car_Id = 0, _Type = ?SYSTEM_PACKET_TIMESTAMP) ->
+    fun read_long_payload/2;
+read_payload_fun(_Car_Id = 0, _Type = ?SYSTEM_PACKET_WEATHER) ->
+    fun read_some_payload/2;
+read_payload_fun(_Car_Id = 0, _Type = ?SYSTEM_PACKET_SPEED) ->
+    fun read_long_payload/2;
+read_payload_fun(_Car_Id = 0, _Type = ?SYSTEM_PACKET_TRACK_STATUS) ->
+    fun read_some_payload/2;
+read_payload_fun(_Car_Id = 0, _Type = ?SYSTEM_PACKET_COPYRIGHT) ->
+    fun read_long_payload/2;
+read_payload_fun(Car_Id, _Type = ?CAR_PACKET_POSITION_UPDATE) when Car_Id > 0 ->
+    fun read_zero_payload/2;
+read_payload_fun(Car_Id, _Type = ?CAR_PACKET_POSITION_HISTORY) when Car_Id > 0 ->
+    fun read_long_payload/2;
+read_payload_fun(Car_Id, Type) when Car_Id > 0, Type >= 1, Type =< 13 ->
+    fun read_some_payload/2.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% `value' field in packets with some payload consists of 3 bits of
+%% <em>data</em> and 4 bits of payload length. If payload length is
+%% `2#1111`, there is no <em>payload</em>. If it's not, that number of
+%% bytes is extracted from the stream of <em>bytes</em>.
+%%
+%% @spec read_some_payload(Value :: integer(), Bytes :: binary()) ->
+%%     {ok, Data :: integer(), Payload :: binary(), Other_Bytes :: binary()} |
+%%     {error, no_match}
+%% @end
+%%--------------------------------------------------------------------
+read_some_payload(Value, Bytes) ->
+    Data = Value band 2#111,
+    Length = Value bsr 3,
+    case Length of
+        2#1111 ->
+            {ok, Data, <<>>, Bytes};
+        _ ->
+            case Bytes of
+                <<Payload:Length/bytes, Other_Bytes/bytes>> ->
+                    {ok, Data, Payload, Other_Bytes};
+                _ ->
+                    {error, no_match}
+            end
+    end.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% The only <em>data</em> in zero payload packet is the `value' field
+%% of the packet. Resulting <em>payload</em> is empty, and stream
+%% <em>bytes</em> are unchanged.
+%%
+%% @spec read_zero_payload(Value :: integer(), Bytes :: binary()) ->
+%%     {ok, Data :: integer(), Payload :: binary(), Other_Bytes :: binary()}
+%% @end
+%%--------------------------------------------------------------------
+read_zero_payload(Value, Bytes) ->
+    {ok, Value, <<>>, Bytes}.
