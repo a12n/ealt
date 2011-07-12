e39a368455964cdc17f98d85bd5722ba93b06d61
Anton Yabchinskiy <arn@users.berlios.de>
2011-07-13 01:49:23 +0400

    start-test.erl, add ealt_translator_0 to event manager

--- bin/start-test.erl
+++ bin/start-test.erl
@@ -9,6 +9,7 @@ main([Email, Password]) ->
     ok = httpc:set_option(cookies, enabled, ealt),
     ok = ealt_auth:login(Email, Password),
     {ok, _Pid_5} = ealt_event_manager:start(),
+    ok = ealt_translator_0:add_handler(),
     {ok, _Pid_6} = ealt_dispatcher:start(),
     {ok, _Pid_7} = ealt_decoder:start(),
     {ok, _Pid_8} = ealt_extractor:start(),
