b0b2094c6f25b8e90a940ef3759aaa3babaa8d2f
Anton Yabchinskiy <arn@users.berlios.de>
2012-01-19 01:57:38 +0400

    app callback module updated

--- src/ealt_app.erl
+++ src/ealt_app.erl
@@ -19,33 +19,20 @@
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
-%% Starts the live timing application. Supervisor and protocol translators are started.
-%%
-%% @spec start(Start_Type, Start_Args) ->
-%%     {ok, Pid :: pid()} |
-%%     {error, Reason :: term()}
+%% Starts the live timing application. Supervisor and protocol
+%% translators are started.
 %% @end
 %%--------------------------------------------------------------------
+-spec start(term(), term()) -> {ok, pid()} | {error, term()}.
 start(_Start_Type, _Start_Args) ->
-    {ok, Email} = application:get_env(ealt, email),
-    {ok, Password} = application:get_env(ealt, password),
-    ok = httpc:set_option(cookies, enabled, ealt),
-    ok = ealt_auth:login(Email, Password),
-    case ealt_sup:start_link() of
-        Result = {ok, _Pid} ->
-            ok = ealt_translator_0:add_handler(),
-            Result;
-        Result = {error, _Reason} ->
-            Result
-    end.
+    ealt_sup:start_link().
 
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
 %% Stops live timing application.
-%%
-%% @spec stop(State) -> void()
 %% @end
 %%--------------------------------------------------------------------
+-spec stop(term()) -> void().
 stop(_State) ->
     ok.
