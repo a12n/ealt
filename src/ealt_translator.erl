569dfc2b465675f8fab6bd871ab3a10af2de2cf3
Anton Yabchinskiy <arn@users.berlios.de>
2013-01-25 16:10:29 +0400

    JSON translator initial implementation

--- src/ealt_translator.erl
+++ src/ealt_translator.erl
@@ -2,8 +2,6 @@
 %%% @author Anton Yabchinskiy <arn@users.berlios.de>
 %%% @copyright See LICENSE file.
 %%% @doc
-%%% Internal protocol revision 0 translations. Just sends internal
-%%% representation of packets.
 %%% @end
 %%%-------------------------------------------------------------------
 -module(ealt_translator).
@@ -17,7 +15,16 @@
 -export([init/1, handle_event/2, handle_call/2,
          handle_info/2, terminate/2, code_change/3]).
 
--record(state, {}).
+-import(erlang, [make_tuple/2]).
+
+%% #state.messages is a proplist. Key is either system message atom
+%% (e.g. wet_track, wind_direction, etc.) or pair of car message atom
+%% (e.g. n_pits, position, etc.) and car id. Value is JSON message
+%% formatted as binary.  #state.fastest_lap accumulates fastest lap
+%% information (car, driver, time and lap), and then all information
+%% is available a single JSON message is issued for clients.
+-record(state, { messages    = [],
+                 fastest_lap = make_tuple(4, undefined) }).
 
 %%%===================================================================
 %%% API
@@ -61,13 +68,20 @@ init(_Args) ->
 %% called for each installed event handler to handle the event.
 %% @end
 %%--------------------------------------------------------------------
-handle_event({client_connected, _Client}, State) ->
-    %% TODO: Format current state to JSON, send to client.
+handle_event({client_connected, Client}, State = #state{ messages = Messages }) ->
+    lists:foreach(fun({_Key, Message}) -> Client(Message) end, Messages),
     {ok, State};
 
-handle_event({message_decoded, _Message}, State) ->
-    %% TODO: Update state, format to JSON, dispatch.
-    {ok, State};
+handle_event({message_decoded, Message}, State) ->
+    {Formatted, Next_State} =
+        handle_message(Message, State),
+    case Formatted of
+        undefined ->
+            ok;
+        _Other ->
+            ealt_websocket:dispatch(Formatted)
+    end,
+    {ok, Next_State};
 
 handle_event(_Event, State) ->
     {ok, State}.
@@ -117,3 +131,109 @@ code_change(_Old_Vsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+-spec handle_message(ealt_message:message(), #state{}) ->
+                            {binary() | undefined, #state{}}.
+
+%% Handle fastest lap system messages. Accumulate data and when all
+%% parts are available, inject fake fastest_lap car message.
+handle_message({FL_Type, Datum}, State = #state{ fastest_lap = FL })
+  when FL_Type =:= fastest_lap_car;
+       FL_Type =:= fastest_lap_driver;
+       FL_Type =:= fastest_lap_lap;
+       FL_Type =:= fastest_lap_time ->
+    Idx_Fun = fun(fastest_lap_car)    -> 1;
+                 (fastest_lap_driver) -> 2;
+                 (fastest_lap_lap)    -> 3;
+                 (fastest_lap_time)   -> 4 end,
+    Next_FL = {Car, Driver, Lap, Time} =
+        setelement(Idx_Fun(FL_Type), FL, Datum),
+    if (Car =/= undefined) and
+       (Driver =/= undefined) and
+       (Lap =/= undefined) and
+       (Time =/= undefined) ->
+            Message = {fastest_lap, Car, Driver, Lap, Time},
+            Next_State = State#state{ fastest_lap = make_tuple(4, undefined) },
+            handle_message(Message, Next_State);
+       true ->
+            Next_State = State#state{ fastest_lap = Next_FL },
+            {undefined, Next_State}
+    end;
+
+handle_message(Message, State = #state{ messages = Messages }) ->
+    {Formatted, Msg_Key} =
+        case element(1, Message) of
+            %% Modifying state system messages
+            SM_Type when SM_Type =:= air_temperature;
+                         SM_Type =:= atmospheric_pressure;
+                         SM_Type =:= copyright;
+                         SM_Type =:= event;
+                         SM_Type =:= relative_humidity;
+                         SM_Type =:= sector_speed;
+                         SM_Type =:= session_clock;
+                         SM_Type =:= session_status;
+                         SM_Type =:= speed_trap;
+                         SM_Type =:= track_temperature;
+                         SM_Type =:= wet_track;
+                         SM_Type =:= wind_direction;
+                         SM_Type =:= wind_speed;
+                         %%
+                         SM_Type =:= fastest_lap ->
+                {format_message(Message), SM_Type};
+            %% Non-modifying state system messages
+            SN_Type when SN_Type =:= commentary;
+                         SN_Type =:= notice ->
+                {format_message(Message), undefined};
+            %% Modifying state car messages
+            CM_Type when CM_Type =:= best_time;
+                         CM_Type =:= driver;
+                         CM_Type =:= gap;
+                         CM_Type =:= interval;
+                         CM_Type =:= lap;
+                         CM_Type =:= n_pits;
+                         CM_Type =:= number;
+                         CM_Type =:= period_time;
+                         CM_Type =:= position;
+                         CM_Type =:= position_history;
+                         CM_Type =:= position_update;
+                         CM_Type =:= sector_time;
+                         %%
+                         CM_Type =:= in_pit;
+                         CM_Type =:= out;
+                         CM_Type =:= retired;
+                         CM_Type =:= stop ->
+                Car = element(2, Message),
+                {format_message(Message), {CM_Type, Car}};
+            %% Non-modifying state car messages
+            CN_Type when CN_Type =:= lap_time ->
+                {format_message(Message), undefined};
+            %% Ignore other messages
+            _Other_Type ->
+                {undefined, undefined}
+        end,
+    Next_Messages =
+        case Msg_Key of
+            undefined ->
+                Messages;
+            _Other_Key ->
+                lists:keystore(Msg_Key, 1, Messages, {Msg_Key, Formatted})
+        end,
+    Next_State =
+        State#state{ messages = Next_Messages },
+    {Formatted, Next_State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+-spec format_message(ealt_message:message()) -> binary().
+
+format_message(_Any) ->
+    %% TODO
+    <<>>.
